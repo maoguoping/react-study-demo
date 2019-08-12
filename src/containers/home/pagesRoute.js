07e8143aee3a766fd43e32bda97ae0adfd4230e2
gpmao <673720904@qq.com>
2019-08-13 00:30:19 +0800

    引入react-loadable，新增路由加载动画

--- src/containers/home/pagesRoute.js
+++ src/containers/home/pagesRoute.js
@@ -1,14 +1,15 @@
-import asyncComponent from '../../components/module/asycComponent';
-const AsyncUserList = asyncComponent(() => import('../pages/userManagement/userList'));
-const AsyncRoleList = asyncComponent(() => import('../pages/userManagement/roleList'));
-const AsyncRightList = asyncComponent(() => import('../pages/userManagement/rightList'));
-const AsyncDeviceList = asyncComponent(() => import('../pages/deviceManagement/deviceList'));
-const AsyncDeviceEventsList = asyncComponent(() => import('../pages/deviceManagement/deviceEventsList'));
+import Loadable from 'react-loadable'
+import { Spin } from 'antd'
+const UserList = Loadable({loader: () => import('../pages/userManagement/userList'), loading: Spin});
+const RoleList = Loadable({loader: () => import('../pages/userManagement/roleList'), loading: Spin});
+const RightList = Loadable({loader: () => import('../pages/userManagement/rightList'), loading: Spin});
+const DeviceList = Loadable({loader: () => import('../pages/deviceManagement/deviceList'), loading: Spin});
+const DeviceEventsList = Loadable({loader: () => import('../pages/deviceManagement/deviceEventsList'), loading: Spin});
 export const defaultPage = {
     name: 'userList',
     path: '/managerCenter/userList',
     menuPath: ['1','sub1','child1'],
-    component: AsyncUserList,
+    component: UserList,
     exact: true
 };
 export const pageRouteList = [
@@ -16,35 +17,35 @@ export const pageRouteList = [
         name: 'userList',
         path: '/managerCenter/userList',
         menuPath: ['1','sub1','child1'],
-        component: AsyncUserList,
+        component: UserList,
         exact: true
     },
     {
         name: 'roleList',
         path: '/managerCenter/roleList',
         menuPath: ['1','sub1','child2'],
-        component: AsyncRoleList,
+        component: RoleList,
         exact: true
     },
     {
         name: 'rightList',
         path: '/managerCenter/rightList',
         menuPath: ['1','sub1','child3'],
-        component: AsyncRightList,
+        component: RightList,
         exact: true
     },
     {
         name: 'deviceList',
         path: '/managerCenter/deviceList',
         menuPath: ['1','sub2','child4'],
-        component: AsyncDeviceList,
+        component: DeviceList,
         exact: true
     },
     {
         name: 'deviceEventsList',
         path: '/managerCenter/deviceEventsList',
         menuPath: ['1','sub2','child5'],
-        component: AsyncDeviceEventsList,
+        component: DeviceEventsList,
         exact: true
     }
 ];
